df8565731eab32bed0ca79019ad1238144c3ac6c
Andras <andras@acm.org>
2015-02-22 17:29:24 -0500

    document all calls, finish unit tests, tweak semantics; 0.9.0

--- Readme.md
+++ Readme.md
@@ -3,75 +3,171 @@ aflow
 
 simple, quick little serial flow control package
 
-## Install
+All functions run sequentially and stop immediately on error.
+
+
+## Installation
 
         npm install aflow
+        npm test aflow
 
-or `git clone git://github.com/andrasq/node-aflow`
 
-## Calls
+## Functions
 
-### repeatUntil( function, callback )
+### repeatUntil( repeatedFunction(cb), callback(err, stoppedWithValue) )
 
-keep calling function until it returns a truthy value, then return that value
-via callback.
+Keep calling the repeated function until it returns a truthy value, then
+return that value via the callback.  Stops looping on error.  This call is
+recursion-safe, it periodically yields to the event loop with setImmediate().
+The repeated call must call its callback, the loop will wait indefinitely and
+will not time out.
 
         var aflow = require('aflow');
         var count = 0;
         aflow.repeatUntil(
-            function(cb) {
+            function repeated(cb) {
                 count += 1;
-                var stopNow = count >= 10 ? count : false;
-                cb(null, stopNow);
+                if (count < 10) cb(null, false);
+                else cb(null, count);
             },
-            function(err, stoppingCount) {
-                console.log("stoppingCount =", count);
+            function whenDone(err, stoppingCount) {
+                console.log("stoppingCount = %d, count = %d", stoppingCount, count);
+                // => stoppingCount = 10, count = 10
             }
-        )
-        // stoppingCount => 10
+        );
 
-### repeatWhile( loopCondition, function, callback )
+### repeatWhile( loopTest, repeatedFunction(cb), callback(err) )
 
-keep calling function as long as loopCondition holds.
+Keep calling repeated function as long as the loopTest() function returns
+truthy.  Stops looping on error.  Any value returned by the repeated function
+is ignored.
 
         var aflow = require('aflow');
         var count = 0;
         aflow.repeatWhile(
-            function() {
+            function loopTest() {
                 return count < 10;
             },
-            function(cb) {
+            function repeated(cb) {
                 count += 1;
                 cb();
             },
-            function(err) {
+            function whenDone(err) {
                 console.log("final count =", count);
-            }
-        )
-        // => final count = 10
-
-### iterate( functionList, callback )
-
-call each function in turn.  Each function is passed two arguments, a
-callback and the returned value from the previous function.  An error
-will interrupt the flow.  Callback will be called with the returned
-value from the last function, or the error.
-
-        var aflow = require('aflow');
-        aflow.iterate([
-            function(cb, prev) {
-                console.log("first; previous", prev); cb(null, 1); },
-            function(cb, prev) {
-                console.log("second; previous", prev); cb(null, 2); },
-            function(cb, prev) {
-                console.log("third; previous", prev); cb(null, 3); }
-            ],
-            function(err, last) {
-                console.log("done; last", last);
-                // last => 3
+                // => final count = 10
             }
         );
-        // => first; previous undefined
-        // => second; previous 1
-        // => third; previous 2
-        // => done; last 3
+
+### applyVisitor( dataItems, visitorFunction(item, cb), callback(err) )
+
+Invoke the visitor function on each data item in turn.  Stops on error.
+
+    var aflow = require('aflow');
+    var visitedItems = [];
+    aflow.applyVisitor(
+        [1,2,3,4],
+        function visitor(item, cb) {
+            visitedItems.push(2 * item);
+            cb();
+        },
+        function whenDone(err) {
+            // visitedItems = [2,4,6,8];
+        }
+    );
+
+### iterate( functionList, callback(err, ret1, ret2) )
+
+Call each function on the list.  The functions are passed three arguments, a
+callback and the first two returned values from the previous function called.
+An error will interrupt the flow.  The when done callback will be called with
+the first two returned values from the last function.  On error, the callback
+will be called with the error and the first two results returned by the call
+that produced the error (if the call returned results).
+
+        var aflow = require('aflow');
+        var allArgs = [];
+        aflow.iterate([
+            function(cb, arg1, arg2) { allArgs.push(arg1); allArgs.push(arg2); cb(null, 1, 11); },
+            function(cb, arg1, arg2) { allArgs.push(arg1); allArgs.push(arg2); cb(null, 2, 22,); },
+            function(cb, arg1, arg2) { allArgs.push(arg1); allArgs.push(arg2); cb(null, 3, 33, 333); },
+            ],
+            function(err, arg1, arg2, arg3) {
+                allArgs.push(arg1);
+                allArgs.push(arg2);
+                allArgs.push(arg3);
+                // allArgs = [undefined, undefined, 1, 11, 2, 22, 3, 33, undefined]
+            }
+        );
+
+
+### reduce( dataItems, subtotal, combineFunction(subtotal, item, cb), callback(err, total) )
+
+Combine all data items with the subtotal using the given combine function.
+The result returned from the last combine function becomes the subtotal passed
+to the next next combine function.  Stops processing on error and returns the
+subtotal so far.  If a combiner function returs a defined subtotal along with
+an error object, that subtotal will be the final subtotal returned.
+
+        var aflow = require('aflow');
+        aflow.reduce(
+            ['a', 'b', 'c', 'd'],
+            ':',
+            function concatenate(subtotal, item, cb) {
+                cb(null, subtotal + item);
+            },
+            function whenDone(err, total) {
+                // total = ":abcd"
+            }
+        );
+
+### map( dataItems, transformFunction(item, cb), callback(err, transformedItems) )
+
+Apply the transformation to each data item, and return the list of transformed
+items.  The transfor function is provided the item and a callback, and should
+call its callback with any error and the transformed item.  Stops and returns
+the partial results so far on error.  If the transform returns an error object
+along with a defined result, it will be included in the partial results.
+
+        var aflow = require('aflow');
+        aflow.map(
+            [1,2,3,4,5],
+            function double(item, cb) {
+                cb(null, 2 * item);
+            },
+            function whenDone(err, items) {
+                // items = [2,4,6,8,10]
+            }
+        );
+
+### filter( dataItems, selectFunction(item, cb), callback(err, selectedItems) )
+
+Evaluate each data item with the select function and return the list of items
+that were selected.  The select function is provided the item and a callback,
+and should call its callback with any error and a truthy value to select that
+item.  If the select function returns an error object along with a truthy
+selection flag, the item generating the error will be included in the partial
+results.
+
+        var aflow = require('aflow');
+        aflow.filter(
+            [1,2,3,4,5],
+            function oddItems(item, cb) {
+                if (item % 2) cb(null, true);
+                else cb(null, false);
+            },
+            function whenDone(err, items) {
+                // items = [1,3,5]
+            }
+        );
+
+## ChangeLog
+
+0.9.0
+
+- document all calls
+- better unit test coverage
+- allow applyVisitor() to iterate any object with a numeric length, eg `arguments` or a `Buffer`
+
+## Todo
+
+- benchmark the speed of each of the primitives
--- test/test-qflow.js
+++ test/test-qflow.js
@@ -12,6 +12,11 @@ module.exports = {
         t.done();
     },
 
+    'should parse package.json': function(t) {
+        require('../package.json');
+        t.done();
+    },
+
     'repeatUntil': {
         'should trap errors and return them in callback': function(t) {
             t.expect(2);
@@ -20,7 +25,7 @@ module.exports = {
                     throw new Error("die");
                 },
                 function(err, last) {
-                    t.ok(err);
+                    t.ok(err instanceof Error);
                     t.equal(err.message, "die");
                     t.done();
                 }
@@ -56,5 +61,264 @@ module.exports = {
                 }
             );
         }
+    },
+
+    'repeatWhile': {
+        'should test loop condition at top': function(t) {
+            var ncalls = 0;
+            t.expect(1);
+            qflow.repeatWhile(
+                function() { return false; },
+                function(next) { ncalls += 1; next(); },
+                function(err) {
+                    t.equal(ncalls, 0);
+                    t.done();
+                }
+            );
+        },
+
+        'should loop n times': function(t) {
+            var ncalls = 0;
+            t.expect(1);
+            qflow.repeatWhile(
+                function() { return ncalls < 1234; },
+                function(next) { ncalls += 1; next(); },
+                function(err) {
+                    t.equal(ncalls, 1234);
+                    t.done();
+                }
+            );
+        },
+
+        'should stop loop on error': function(t) {
+            var ncalls = 0;
+            t.expect(3);
+            qflow.repeatWhile(
+                function() { return ncalls < 1234; },
+                function(next) { ncalls += 1; if (ncalls < 111) next(); else throw new Error("stop at 111"); },
+                function(err) {
+                    t.equal(ncalls, 111);
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "stop at 111");
+                    t.done();
+                }
+            );
+        },
+    },
+
+    'applyVisitor': {
+        'should call visitor with each item': function(t) {
+            var visited = [];
+            qflow.applyVisitor(
+                [1,2,3,4],
+                function(item, cb) {
+                    visited.push(item);
+                    cb();
+                },
+                function(err) {
+                    t.deepEqual(visited, [1,2,3,4]);
+                    t.done();
+                }
+            );
+        },
+
+        'should return error and stop looping on error': function(t) {
+            var visited = [];
+            t.expect(3);
+            qflow.applyVisitor(
+                [1,2,3,4],
+                function(item, cb) {
+                    visited.push(item);
+                    if (item == 3) throw new Error("stop at 3");
+                    cb();
+                },
+                function(err) {
+                    t.deepEqual(visited, [1,2,3]);
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "stop at 3");
+                    t.done();
+                }
+            );
+        },
+    },
+
+    'iterate': {
+        'should call each function': function(t) {
+            var items = [];
+            t.expect(1);
+            qflow.iterate([
+                function(cb) { items.push(1); cb() },
+                function(cb) { items.push(2); cb() },
+                function(cb) { items.push(3); cb() },
+                ],
+                function(err) {
+                    t.deepEqual(items, [1,2,3]);
+                    t.done();
+                }
+            );
+        },
+
+        'should pass previous result to each next function and return the last result': function(t) {
+            var args = [];
+            t.expect(3);
+            qflow.iterate([
+                function(cb, arg, arg2) { args.push(arg); args.push(arg2); cb(null, 1, 11); },
+                function(cb, arg, arg2) { args.push(arg); args.push(arg2); cb(null, 2, 22); },
+                function(cb, arg, arg2) { args.push(arg); args.push(arg2); cb(null, 3, 33); },
+                ],
+                function(err, arg, arg2) {
+                    t.deepEqual(args, [undefined, undefined, 1, 11, 2, 22]);
+                    t.equal(arg, 3);
+                    t.equal(arg2, 33);
+                    t.done();
+                }
+            );
+        },
+
+        'should stop on error': function(t) {
+            var items = [];
+            t.expect(3);
+            qflow.iterate([
+                function(cb) { items.push(1); cb(); },
+                function(cb) { items.push(2); throw new Error("stop at 2"); cb() },
+                function(cb) { items.push(3); cb() },
+                ],
+                function(err) {
+                    t.deepEqual(items, [1,2]);
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "stop at 2");
+                    t.done();
+                }
+            );
+        },
+    },
+
+    'reduce': {
+        'should combine items': function(t) {
+            var items = [1,2,3,4,5];
+            t.expect(1);
+            qflow.reduce(
+                items,
+                '',
+                function(total, item, cb) {
+                    total += '.' + item;
+                    cb(null, total);
+                },
+                function(err, final) {
+                    t.equal(final, '.1.2.3.4.5');
+                    t.done();
+                }
+            );
+        },
+
+        'returns errors along with partial results': function(t) {
+            var items = [1,2,3,4,5];
+            t.expect(3);
+            qflow.reduce(
+                items,
+                '',
+                function(total, item, cb) {
+                    if (item === 3) throw new Error("err at 3");
+                    total += '.' + item;
+                    cb(null, total);
+                },
+                function(err, final) {
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "err at 3");
+                    t.equal(final, ".1.2");
+                    t.done();
+                }
+            );
+        },
+    },
+
+    'map': {
+        'should apply function to all items': function(t) {
+            var items = [1,2,3,4];
+            t.expect(1);
+            qflow.map(
+                items,
+                function(item, cb) { cb(null, item + item); },
+                function(err, mapped) {
+                    t.deepEqual(mapped, [2,4,6,8]);
+                    t.done();
+                }
+            );
+        },
+
+        'should return error and partial results': function(t) {
+            var items = [1,2,3,4];
+            t.expect(3);
+            qflow.map(
+                items,
+                function(item, cb) { if (item === 3) throw new Error("three"); else cb(null, item + item); },
+                function(err, mapped) {
+                    t.deepEqual(mapped, [2,4]);
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "three");
+                    t.done();
+                }
+            );
+        },
+
+        'should include a defined item that came with error': function(t) {
+            var items = [1,2,3,4];
+            t.expect(1);
+            qflow.map(
+                items,
+                function(item, cb) { cb((item === 3 ? new Error("three") : null), item); },
+                function(err, mapped) {
+                    t.equal(mapped[2], 3);
+                    t.done();
+                }
+            );
+        },
+    },
+
+    'filter': {
+        'should return array of items selected by filter function': function(t) {
+            var items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+            t.expect(1);
+            qflow.filter(
+                items,
+                function(item, cb) {
+                    // odd multiples of 3
+                    var yesInclude = (item % 2) && (item % 3 == 0)
+                    cb(null, yesInclude);
+                },
+                function(err, found) {
+                    t.deepEqual(found, [3, 9, 15]);
+                    t.done();
+                }
+            );
+        },
+
+        'should return any errors': function(t) {
+            var items = [1,2,3,4];
+            t.expect(3);
+            qflow.filter(
+                items,
+                function(item, cb) { if (item == 3) throw new Error("err on 3"); cb(null, true); },
+                function(err, found) {
+                    t.ok(err instanceof Error);
+                    t.equal(err.message, "err on 3");
+                    t.deepEqual(found, [1,2]);
+                    t.done();
+                }
+            );
+        },
+
+        'should include a defined item that came with error': function(t) {
+            var items = [1,2,3,4];
+            t.expect(1);
+            qflow.filter(
+                items,
+                function(item, cb) { cb((item === 3 ? new Error("three") : null), item); },
+                function(err, picked) {
+                    t.equal(picked[2], 3);
+                    t.done();
+                }
+            );
+        },
     },
 };
